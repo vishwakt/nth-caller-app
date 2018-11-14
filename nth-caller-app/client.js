b29cc95e963262788ec2e7259b79310849990fa3
vishwakt <vishwakt@gmail.com>
2018-11-14 14:42:04 -0800

    Added call by gameID

--- nth-caller-app/client.js
+++ nth-caller-app/client.js
@@ -3,8 +3,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let newGameId = document.getElementById('new-game-id')
     let n = document.getElementById('n')
+    let callGameId = document.getElementById('call-game-id')
+    let name = document.getElementById('name')
+    let resultP = document.getElementById('status')
 
     document.getElementById('register').addEventListener('click', register)
+    document.getElementById('call').addEventListener('click', call)
 
     function register() {
         if (newGameId.value === "" || n.value === "") {
@@ -35,5 +39,26 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         return fetch(route, request)
         .then(res => {return res.json()})
+    }
+
+    function call(){
+        if (callGameId.value === "") {
+            console.log("GameID is is required. No call was made.")
+            return
+        }
+
+        let body = {
+            id: callGameId.value,
+            name: name.value,
+        }
+
+        makePost('/call', body)
+        .then(data => {
+            if (!data.success) {
+                console.log("No such game found. Enter valid game ID")
+            } else {
+                resultP.innerHTML = data.message
+            }
+        })
     }
 })
